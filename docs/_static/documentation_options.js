620188d45f83aceea539bf69a39dd1250708ec8c
Peter Mackenzie-Helnwein <pmackenz@uw.edu>
2023-03-03 00:17:44 -0800

    version update

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4',
+    VERSION: 'FEM.edu-v0.9-pre3.2.23',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
