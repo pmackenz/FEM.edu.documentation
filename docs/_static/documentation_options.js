f8603c2000637604158317da821e14d60ea37d98
Peter Mackenzie-Helnwein <pmackenz@uw.edu>
2023-03-18 21:24:41 -0700

    introducing galleries

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'FEM.edu-v0.9-pre3.8.23',
+    VERSION: 'FEM.edu-v0.9.2-pre',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
