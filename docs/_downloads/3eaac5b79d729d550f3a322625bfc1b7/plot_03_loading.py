eac4c7a073cf3a8323d11468f621f6503ecead76
Peter Mackenzie-Helnwein <pmackenz@uw.edu>
2025-01-12 10:58:18 -0800

    adding the mesher tutorial

---
+++ docs/_downloads/3eaac5b79d729d550f3a322625bfc1b7/plot_03_loading.py
@@ -0,0 +1,16 @@
+"""
+.. _loading_a_model_03:
+
+===================================================
+Tutorial 3 - Loading a Model
+===================================================
+This tutorial demonstrated loading techniques in |Application|.
+"""
+# %%
+#
+# ------------------------
+# The preparation stage
+# ------------------------
+# Before building a model, we need to load the used components.
+# Every model needs one :py:class:`System` instance.  Furthermore,
+# we are going to use several :py:class:`Node` and :py:class:`Element` instances.
